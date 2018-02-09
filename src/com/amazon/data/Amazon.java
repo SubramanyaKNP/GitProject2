5d958fe1adb11f3810bcb852abc4264e7a0db161
subramanya knp <subramanyaknp@gmail.com>
2018-02-09 19:54:26 +0530

    its committed

---
+++ src/com/amazon/data/Amazon.java
@@ -0,0 +1,10 @@
+package com.amazon.data;
+
+public class Amazon {
+
+	public static void main(String[] args) {
+		System.out.println("amazon class");
+
+	}
+
+}
---
+++ src/com/flipkart/data/Flipkart.java
@@ -0,0 +1,11 @@
+package com.flipkart.data;
+
+public class Flipkart {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("flipkart class");
+	}
+
+}
